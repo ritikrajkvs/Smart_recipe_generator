f1d541d5ef47e3793b23d7fc1868d4545c73d5af
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-11-22 02:23:28 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,20 +8,21 @@ export default {
     extend: {
       // Custom animation keyframes for floating elements
       animation: {
-        blob: "blob 7s infinite",
+        blob: "blob 10s infinite", // Slower = smoother
         float: "float 6s ease-in-out infinite",
       },
       keyframes: {
         blob: {
-          "0%": { transform: "translate(0px, 0px) scale(1)" },
-          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
-          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
-          "100%": { transform: "translate(0px, 0px) scale(1)" },
+          // translate3d forces GPU acceleration
+          "0%": { transform: "translate3d(0px, 0px, 0px) scale(1)" },
+          "33%": { transform: "translate3d(30px, -50px, 0px) scale(1.1)" },
+          "66%": { transform: "translate3d(-20px, 20px, 0px) scale(0.9)" },
+          "100%": { transform: "translate3d(0px, 0px, 0px) scale(1)" },
         },
         float: {
-          "0%": { transform: "translateY(0px) rotate(0deg)" },
-          "50%": { transform: "translateY(-20px) rotate(10deg)" },
-          "100%": { transform: "translateY(0px) rotate(0deg)" },
+          "0%": { transform: "translate3d(0, 0, 0) rotate(0deg)" },
+          "50%": { transform: "translate3d(0, -20px, 0) rotate(10deg)" },
+          "100%": { transform: "translate3d(0, 0, 0) rotate(0deg)" },
         },
       },
     },
