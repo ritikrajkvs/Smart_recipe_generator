e92742fd4757e8141e169e232364587bdd1c5635
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-12-11 17:20:09 +0530

    Update userSlice.js

--- frontend/src/utils/userSlice.js
+++ frontend/src/utils/userSlice.js
@@ -10,8 +10,19 @@ const userSlice = createSlice({
     removeUser: (state, action) => {
       return null;
     },
+    // Optimistic updates for favorites
+    addFavoriteID: (state, action) => {
+      if (state && state.favorites) {
+        state.favorites.push(action.payload);
+      }
+    },
+    removeFavoriteID: (state, action) => {
+      if (state && state.favorites) {
+        state.favorites = state.favorites.filter((id) => id !== action.payload);
+      }
+    },
   },
 });
 
-export const { addUser, removeUser } = userSlice.actions;
+export const { addUser, removeUser, addFavoriteID, removeFavoriteID } = userSlice.actions;
 export default userSlice.reducer;
