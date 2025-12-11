fd2a3f7ad0f4e540fe6cac379d0b05ea47aed4cf
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-12-11 17:21:23 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { addUser } from './utils/userSlice';
-import { setFavorites } from './utils/recipeSlice';
 
 import Home from './pages/Home';
 import Results from './pages/Results';
@@ -15,7 +14,6 @@ import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 
-// Layout component to handle initial data fetching
 const AppLayout = () => {
   const dispatch = useDispatch();
 
@@ -27,14 +25,9 @@ const AppLayout = () => {
           const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/auth/user`, {
             headers: { 'x-auth-token': token }
           });
-          // Dispatch user data to Redux
           dispatch(addUser(res.data));
-          // Dispatch favorites to Redux
-          if (res.data.favorites) {
-            dispatch(setFavorites(res.data.favorites));
-          }
         } catch (err) {
-          console.error("Session load failed", err);
+          console.error("Session invalid", err);
           localStorage.removeItem('token');
         }
       }
@@ -57,18 +50,15 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AppLayout />}>
-          {/* Public Routes */}
           <Route path='login' element={<Login />} />
           <Route path='signup' element={<Signup />} />
 
-          {/* Protected Routes */}
-          <Route index element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path='results' element={<ProtectedRoute><Results /></ProtectedRoute>} />
           <Route path='recipe/:id' element={<ProtectedRoute><RecipeDetails /></ProtectedRoute>} />
           <Route path='favorites' element={<ProtectedRoute><Favorites /></ProtectedRoute>} />
           <Route path='suggested' element={<ProtectedRoute><Suggested /></ProtectedRoute>} />
 
-          {/* Catch All */}
           <Route path='*' element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
